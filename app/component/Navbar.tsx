552f5b3300ba9baa71629db6ae2cb30d329172f1
velpcode <velpmail@gmail.com>
2024-05-26 15:42:26 -0400

    navbarlinks

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
         <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
             <div className="md:col-span-3">
                 <Link href = "/">
-                    <h1>ShopVlp</h1>
+                    <h1 className="font-bold text-xl">Shop<span className="text-violet-500">Vlp</span></h1>
                 </Link>
                 <a href = ""></a>
             </div>
