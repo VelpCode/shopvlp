4d85593653b60373649b4fc7fb0f74dc1345c12a
velpcode <velpmail@gmail.com>
2024-06-05 19:32:09 -0400

    hol

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,5 +1,5 @@
 
-
+import { Card, CardHeader, CardTitle,  } from "@/components/ui/card"
 
 export default function SellRoute() {
 
@@ -7,8 +7,17 @@ export default function SellRoute() {
 
         <section className="max-w-7xl mx-auto px-4 md:px-8">
             
+            <Card>
+                <form>
+                    <CardHeader>
+                        <CardTitle>
+                            
+                        </CardTitle>
+                    </CardHeader>
+                </form>
+            </Card>
+
         </section>
-        
     )
 
 
